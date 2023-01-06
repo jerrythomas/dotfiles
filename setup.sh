fc8d5b3f4e429a999f4e21c85bb1e78f59356e2e
Jerry Thomas <jerrythomas@users.noreply.github.com>
2023-01-06 12:52:15 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@
 # rm -rf $TMPDIR
 
 ln -s ~/.dotfiles/.spaceshiprc.zsh ~/.spaceshiprc.zsh
+ln -s ~/.dotfiles/utils.zsh ~/utils.zsh
 mkdir ~/.nvm
 echo "source ~/.dotfiles/.zshrc" >>! ~/.zshrc
 $(brew --prefix)/bin/zsh
