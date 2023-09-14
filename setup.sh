211f185707bd56fbc71e8d180f13f6ac2c7af576
Jerry Thomas <jerrythomas@users.noreply.github.com>
2023-09-14 11:34:33 +0530

    Added bun installer to setup.

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ ln -s ~/.dotfiles/.spaceshiprc.zsh ~/.spaceshiprc.zsh
 ln -s ~/.dotfiles/utils.zsh ~/utils.zsh
 mkdir ~/.nvm
 echo "source ~/.dotfiles/.zshrc" >>! ~/.zshrc
+curl -fsSL https://bun.sh/install | bash
 $(brew --prefix)/bin/zsh
